0f818c1286f5a6d0ee502f29b6e9fd8b85dd060c
anilyavas <anil.yavas99@gmail.com>
2024-03-14 15:16:34 +0300

    expo location

--- App.tsx
+++ App.tsx
@@ -1,26 +1,47 @@
 import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import * as Location from 'expo-location';
 
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
 const OPEN_WEATHER_KEY = process.env.EXPO_PUBLIC_OPEN_WEATHER_KEY;
 
 const WeatherScreen = () => {
   const [weather, setWeather] = useState<Weather>();
+  const [location, setLocation] = useState<Location.LocationObject>(null);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    if (location) {
+      fetchWeather();
+    }
+  }, [location]);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   const fetchWeather = async () => {
-    const lat = 44.34;
-    const lon = 10.99;
+    if (!location) {
+      return;
+    }
+
     //fetch data
     const results = await fetch(
-      `${BASE_URL}?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_KEY}&units=metric`
+      `${BASE_URL}?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${OPEN_WEATHER_KEY}&units=metric`
     );
     const data = await results.json();
     console.log(JSON.stringify(data, null, 2));
     setWeather(data);
   };
-
-  useEffect(() => {
-    fetchWeather();
-  }, []);
 
   if (!weather) {
     return <ActivityIndicator />;
@@ -29,7 +50,7 @@ const WeatherScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.location}>{weather.name}</Text>
-      <Text style={styles.temp}>{Math.round(weather.main.temp)}</Text>
+      <Text style={styles.temp}>{Math.round(weather.main.temp)}°</Text>
     </View>
   );
 };
